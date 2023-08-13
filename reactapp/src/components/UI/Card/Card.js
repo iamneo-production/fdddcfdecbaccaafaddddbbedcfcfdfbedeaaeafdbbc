007d1bdb8935ed28217db9ea1bc217b21e88dace
Iamneo <techadmin@iamneo.ai>
2023-08-13 09:38:55 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -3,20 +3,26 @@ import Button from "../Button/Button";
 const Card = ({ question, correctAnswerMarkUpdate, attempt, options,answer,setQsns,qsn ,setCard,qsnAttempt}) => {
 // Creating an array of options
 const optionsArray = [options.option1, options.option2, options.option3,options.option4];
+// State variable to track the marked status of the button
+const [marked, setMarked] = useState(false);
+
+const click = (clicked) => {
 
 setQsns(qsn+1)
 
 setMarked(true)
 if (qsnAttempt === 4) {
-
+// Show the result if all question are attempted
 setCard(true)
 }
 if (clicked === answer)
-
+// Increase the score if the answer is correct
 correctAnswerMarkUpdate(attempt + 1)
 }
-
-const norm = "bg-red-300 px-2 py-1 rounded hover:bg-orange-400"const disable = "bg-orange-300 px-2 py-1 rounded disabled:opacity-50"return (
+// Styles for the button
+const norm = "bg-red-300 px-2 py-1 rounded hover:bg-orange-400"
+const disable = "bg-orange-300 px-2 py-1 rounded disabled:opacity-50"
+return (
 <div>
 <h4 className="pt-3 pb-1">{question}</h4>
 <div className="flex space-x-2">
